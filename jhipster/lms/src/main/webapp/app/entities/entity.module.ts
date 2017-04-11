0f767eaf5f3de58128db7932bcd49bbc411838ba
asaxena_ciphercloud <asaxena@ciphercloud.com>
2017-04-10 20:07:25 -0700

    Jhipster regenerate

--- jhipster/lms/src/main/webapp/app/entities/entity.module.ts
+++ jhipster/lms/src/main/webapp/app/entities/entity.module.ts
@@ -1,13 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { LmsLocalFileModule } from './local-file/local-file.module';
-import { LmsArchiveJobModule } from './archive-job/archive-job.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        LmsLocalFileModule,
-        LmsArchiveJobModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
