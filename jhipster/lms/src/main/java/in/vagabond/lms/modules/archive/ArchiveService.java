336bc209f1bb6dd9e4ec0bba7aff92ba87763e5d
asaxena_ciphercloud <asaxena@ciphercloud.com>
2017-04-10 19:37:57 -0700

    Added simple file crawling service

--- jhipster/lms/src/main/java/in/vagabond/lms/modules/archive/ArchiveService.java
+++ jhipster/lms/src/main/java/in/vagabond/lms/modules/archive/ArchiveService.java
@@ -8,4 +8,5 @@ import java.io.IOException;
 public interface ArchiveService {
 
     public void archive() throws IOException;
+    public boolean isRunning();
 }
--- jhipster/lms/src/main/java/in/vagabond/lms/modules/archive/impl/ArchiveServiceImpl.java
+++ jhipster/lms/src/main/java/in/vagabond/lms/modules/archive/impl/ArchiveServiceImpl.java
@@ -4,6 +4,7 @@ package in.vagabond.lms.modules.archive.impl;
 import in.vagabond.lms.modules.archive.ArchiveService;
 import in.vagabond.lms.service.LocalFileService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -19,7 +20,11 @@ import java.nio.file.Paths;
 
 @Component
 @Slf4j
+
 public class ArchiveServiceImpl implements ArchiveService{
+
+
+    private boolean running = false;
 
 
     private Path startPath = Paths.get("/Archive/Inbox");
@@ -31,10 +36,23 @@ public class ArchiveServiceImpl implements ArchiveService{
 
     }
 
+    @Async
     public void archive() throws IOException {
         log.info("----");
         log.info("Starting archive operation");
-        Files.walkFileTree(startPath, new RulesBasedFileVisitor<Path>());
+        running = true;
+        try {
+            Files.walkFileTree(startPath, new RulesBasedFileVisitor<Path>());
+        }catch (IOException ioe) {
+            throw ioe;
+        } finally {
+            running = false;
+        }
+    }
+
+    public boolean isRunning() {
+
+        return running;
 
     }
 
